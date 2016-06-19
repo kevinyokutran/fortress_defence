59c9da35ab36de57fe045a33138004cdacbf35da
Josh Vocal <joshvocal@gmail.com>
2016-06-19 10:41:23 -0700

    Added currentBoard and boardAfterGame

--- FortressDefence/src/GameUI/GameUI.java
+++ FortressDefence/src/GameUI/GameUI.java
@@ -1,4 +1,31 @@
 package GameUI;
 
+import GameLogic.Board;
+import java.util.List;
+
 public class GameUI {
+
+    private Board board;
+
+    public GameUI(Board board) {
+        this.board = board;
+    }
+
+    public void printDamageFromTanks(List<Integer> damageOfTanks) {
+        for (Integer damage : damageOfTanks) {
+            if (damage > 0) {
+                System.out.printf("You were shot for %d!%n", damage);
+            }
+        }
+    }
+
+    public void printFortressHealth(int health) {
+        System.out.printf("Fortress Structure Left: %d.%n", health);
+    }
+
+    /* Todo */
+    public void getUserCoordinates() {
+        System.out.print("Enter your move: ");
+        System.out.println();
+    }
 }
--- FortressDefence/src/Main.java
+++ FortressDefence/src/Main.java
@@ -1,5 +1,6 @@
 import GameLogic.Board;
 import GameUI.BoardUI;
+import GameUI.GameUI;
 
 public class Main {
 
@@ -8,14 +9,22 @@ public class Main {
     private static final int NUMBER_OF_TANKS = 5;
     private static final int NUMBER_OF_CELLS = 4;
 
+    private static int health = 1500;
+
     public static void main(String[] args) {
+        // Testing functions here for game.
         Board board = new Board(ROW, COLUMN, NUMBER_OF_TANKS, NUMBER_OF_CELLS);
-        BoardUI ui = new BoardUI(board);
-        ui.displayBoard();
+        BoardUI boardUI = new BoardUI(board);
+        GameUI gameUI = new GameUI(board);
+        boardUI.displayBoard();
+        gameUI.printFortressHealth(health);
+        gameUI.getUserCoordinates();
+        gameUI.printDamageFromTanks(board.getDamageFromTanks());
         System.out.println();
-        
-        board.getCell(1, 2).setIsMissed(); // Test line for user to select and miss a tank cell.
-        ui.displayBoardAfterGame();
+
+        board.getCell(9, 9).setIsMissed();
+        boardUI.displayBoardAfterGame();
+
     }
 
 }
