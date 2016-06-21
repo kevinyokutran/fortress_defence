3aa6cab369fd1e55f2f639878a1ff8e90691d446
ktraain <kevinyokutran@gmail.com>
2016-06-20 18:32:20 -0700

    Removed testing print statement

--- FortressDefence/src/GameLogic/Board.java
+++ FortressDefence/src/GameLogic/Board.java
@@ -165,7 +165,6 @@ public class Board {
         }
         else {
             cell.setIsMissed();
-            System.out.println(tanks[0].getCells());
         }
         cell.setIsKnownToPlayer();
     }
